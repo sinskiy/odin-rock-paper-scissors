db75fc4afd59274187146482157ac05619325e2c
sinskiy <sinskiy@proton.me>
2024-06-06 21:01:23 +0300

    add functions to get choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const tools = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  // get floated from 0 to 2 and round it
+  const randomToolIndex = Math.floor(Math.random() * 3);
+  return tools[randomToolIndex];
+}
+
+function getHumanChoice() {
+  const randomTool = prompt("Choose a tool");
+  return randomTool;
+}
