02c496aa52a8b3fad087b8fb62f0cfc9cf1eb2f5
sinskiy <sinskiy@proton.me>
2024-06-06 21:06:57 +0300

    check if user entered correct tool case-insensitive

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const tools = ["rock", "paper", "scissors"];
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {}
+
 function getComputerChoice() {
   // get floated from 0 to 2 and round it
   const randomToolIndex = Math.floor(Math.random() * 3);
@@ -7,6 +12,9 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const choice = prompt("Choose a tool");
+  let choice = "";
+  do {
+    choice = prompt("Choose a tool: Rock, Paper, Scissors").toLowerCase();
+  } while (!tools[choice]);
   return choice;
 }
